509d4767258ca709e699a1464a206e882a95a59a
Mike Yan <yiyanz@amazon.com>
2022-03-24 17:33:27 +0800

    feat: add OCR shipped-from

--- source/main.ts
+++ source/main.ts
@@ -2,23 +2,28 @@
 import 'source-map-support/register';
 import {App} from '@aws-cdk/core';
 import {BootstraplessStackSynthesizer} from 'cdk-bootstrapless-synthesizer';
-import {PornImageStack} from './porn-image/porn-image';
-import {InferOCRStack} from './infer-ocr/infer-ocr';
-import {HumanSegStack} from './human-seg/human-seg';
-import {SuperResolutionStack} from './super-resolution/super-resolution';
-import {SuperResolutionInf1Stack} from './super-resolution-inf1/super-resolution-inf1';
-import {SuperResolutionGpuStack} from './super-resolution-gpu/super-resolution-gpu';
-import {OCRBusinessLicenseSolutionCDKStack} from './ocr-cn-business-license/template/ocr-business-license-cdk-stack';
+// import {PornImageStack} from './porn-image/porn-image';
+// import {InferOCRStack} from './infer-ocr/infer-ocr';
+// import {HumanSegStack} from './human-seg/human-seg';
+// import {SuperResolutionStack} from './super-resolution/super-resolution';
+// import {SuperResolutionInf1Stack} from './super-resolution-inf1/super-resolution-inf1';
+// import {SuperResolutionGpuStack} from './super-resolution-gpu/super-resolution-gpu';
+// import {OCRBusinessLicenseSolutionCDKStack} from './ocr-cn-business-license/template/ocr-business-license-cdk-stack';
+// import {AISolutionKitStack} from './api-deployment/ai-solution-kit-stack';
+import {OCRShippedFromSolutionCDKStack} from './ocr-shipped-from/template/ocr-shipped-from-cdk-stack';
 
 
 const app = new App();
-new PornImageStack(app, "AIKits-Porn-Image-Detection-Stack", {synthesizer: synthesizer()});
-new InferOCRStack(app, "AIKits-Infer-OCR-Stack", {synthesizer: synthesizer()});
-new HumanSegStack(app, "AIKits-Human-Seg-Stack", {synthesizer: synthesizer()});
-new SuperResolutionStack(app, "AIKits-Super-Resolution-Stack", {synthesizer: synthesizer()});
-new SuperResolutionInf1Stack(app, "AIKits-Super-Resolution-Inf1-Stack", {synthesizer: synthesizer()});
-new SuperResolutionGpuStack(app, "AIKits-Super-Resolution-GPU-Stack", {synthesizer: synthesizer()});
-new OCRBusinessLicenseSolutionCDKStack(app, "AIKits-OCR-CN-Business-License-Stack", {synthesizer: synthesizer()});
+// new PornImageStack(app, "AIKits-Porn-Image-Detection-Stack", {synthesizer: synthesizer()});
+// new InferOCRStack(app, "AIKits-Infer-OCR-Stack", {synthesizer: synthesizer()});
+// new HumanSegStack(app, "AIKits-Human-Seg-Stack", {synthesizer: synthesizer()});
+// new SuperResolutionStack(app, "AIKits-Super-Resolution-Stack", {synthesizer: synthesizer()});
+// new SuperResolutionInf1Stack(app, "AIKits-Super-Resolution-Inf1-Stack", {synthesizer: synthesizer()});
+// new SuperResolutionGpuStack(app, "AIKits-Super-Resolution-GPU-Stack", {synthesizer: synthesizer()});
+// new OCRBusinessLicenseSolutionCDKStack(app, "AIKits-OCR-CN-Business-License-Stack", {synthesizer: synthesizer()});
+// new AISolutionKitStack(app, "AISolutionKitStack3", {synthesizer: synthesizer()});
+new OCRShippedFromSolutionCDKStack(app, "AIKits-OCR-Shipped-From-Stack", {synthesizer: synthesizer()});
+
 app.synth()
 
 function synthesizer() {
