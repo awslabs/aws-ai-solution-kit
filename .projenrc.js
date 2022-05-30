00203c7efe6b3f62afd2c6114dda5026f1c23ff1
Mike Yan <yiyanz@amazon.com>
2022-05-30 17:46:37 +0800

    fix: pull request template

--- .projenrc.js
+++ .projenrc.js
@@ -43,6 +43,23 @@ const project = new awscdk.AwsCdkTypeScriptApp({
     'deployment/global-s3-assets/',
     'deployment/regional-s3-assets/',
   ],
+  pullRequestTemplate: true,
+  pullRequestTemplateContents: [
+    '## Description',
+    '',
+    'Please include a summary of the changes and the related issue. Please also include relevant motivation and context. List any dependencies that are required for this change.',
+    '',
+    'Fixes # (issue)',
+    '',
+    '## Type of change',
+    '',
+    'Please delete options that are not relevant.',
+    '',
+    '- [ ] Bug fix (non-breaking change which fixes an issue)',
+    '- [ ] New feature (non-breaking change which adds functionality)',
+    '- [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)',
+    '- [ ] This change requires a documentation update'
+  ],
   watchExcludes: [
     'README.md',
     'cdk*.json',
