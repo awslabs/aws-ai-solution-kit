c2aebcf817901329971ec41ddc7765d4b5ff58bb
Mike Yan <yiyanz@amazon.com>
2022-10-22 22:16:40 +0800

    fix: Deployment issue for features

--- src/api-deployment/lambda-feature-construct.ts
+++ src/api-deployment/lambda-feature-construct.ts
@@ -118,6 +118,7 @@ export class LambdaFeatureConstruct extends Construct {
         removalPolicy: RemovalPolicy.DESTROY,
       });
       apiProvider.node.addDependency(post);
+      deployment.node.addDependency(apiProvider);
     }
   }
 }
