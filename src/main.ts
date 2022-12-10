452feecf3b1bb68b93092ac808ad547d1cd17842
Mike Yan <yiyanz@amazon.com>
2022-12-10 23:06:26 +0800

    feat: update build containers for SageMaker

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,7 @@
 import { App, Aspects } from 'aws-cdk-lib';
 import { BootstraplessStackSynthesizer, CompositeECRRepositoryAspect } from 'cdk-bootstrapless-synthesizer';
 import 'source-map-support/register';
-import { AISolutionKitIsolatedStack } from './api-deployment/ai-solution-kit-isolated-stack';
-import { AISolutionKitSMStack } from './api-deployment/ai-solution-kit-sm-stack';
-import { AISolutionKitGpuStack } from './api-deployment/ai-solution-kit-gpu-stack';
-import { AISolutionKit3GStack } from './api-deployment/ai-solution-kit-3g-stack';
 import { AISolutionKitStack } from './api-deployment/ai-solution-kit-stack';
-import { AISolutionKitOcrBlankStack } from './api-deployment/ai-solution-kit-ocr-blank-stack';
-import { AISolutionKitChineseOCRStack } from './api-deployment/ai-solution-kit-chinese-ocr-stack';
 import { LambdaContainersStack } from './containers/lambda-containers-stack';
 
 
@@ -38,53 +32,6 @@ if (buildContainers === 'true' || deployContainers === 'true') {
     },
   });
 
-  new AISolutionKitIsolatedStack(app, 'AI-Solution-Kit-Isolated', {
-    synthesizer: synthesizer(),
-    ecrRegistry: ecrRegistry === 'undefined' ? 'public.ecr.aws/aws-gcr-solutions/aws-gcr-ai-solution-kit' : ecrRegistry,
-    tags: {
-      app: 'ai-solution-kit',
-    },
-  });
-
-  new AISolutionKitSMStack(app, 'AI-Solution-Kit-SM', {
-    synthesizer: synthesizer(),
-    ecrRegistry: ecrRegistry === 'undefined' ? 'public.ecr.aws/aws-gcr-solutions/aws-gcr-ai-solution-kit' : ecrRegistry,
-    tags: {
-      app: 'ai-solution-kit',
-    },
-  });
-
-  new AISolutionKitGpuStack(app, 'AI-Solution-Kit-Gpu', {
-    synthesizer: synthesizer(),
-    ecrRegistry: ecrRegistry === 'undefined' ? 'public.ecr.aws/aws-gcr-solutions/aws-gcr-ai-solution-kit' : ecrRegistry,
-    tags: {
-      app: 'ai-solution-kit',
-    },
-  });
-
-  new AISolutionKit3GStack(app, 'AI-Solution-Kit-3G', {
-    synthesizer: synthesizer(),
-    ecrRegistry: ecrRegistry === 'undefined' ? 'public.ecr.aws/aws-gcr-solutions/aws-gcr-ai-solution-kit' : ecrRegistry,
-    tags: {
-      app: 'ai-solution-kit',
-    },
-  });
-
-  new AISolutionKitOcrBlankStack(app, 'AI-Solution-Kit-Ocr-Blank', {
-    synthesizer: synthesizer(),
-    ecrRegistry: ecrRegistry === 'undefined' ? 'public.ecr.aws/aws-gcr-solutions/aws-gcr-ai-solution-kit' : ecrRegistry,
-    tags: {
-      app: 'ai-solution-kit',
-    },
-  });
-
-  new AISolutionKitChineseOCRStack(app, 'AI-Solution-Kit-Chinese-Ocr', {
-    synthesizer: synthesizer(),
-    ecrRegistry: ecrRegistry === 'undefined' ? 'public.ecr.aws/aws-gcr-solutions/aws-gcr-ai-solution-kit' : ecrRegistry,
-    tags: {
-      app: 'ai-solution-kit',
-    },
-  });
 }
 
 if (process.env.USE_BSS) {
