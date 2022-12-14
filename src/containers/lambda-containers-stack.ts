264ea0d86cb49b4e5efe6eee5018e7c681c8f1d3
Mike Yan <yiyanz@amazon.com>
2022-12-14 09:39:27 +0800

    fix: fixed advanced OCR for sagemaker model

--- src/containers/lambda-containers-stack.ts
+++ src/containers/lambda-containers-stack.ts
@@ -13,7 +13,7 @@ import 'source-map-support/register';
 export class LambdaContainersStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
+    
     const GeneralOCRAdvanced = new DockerImageFunction(
       this,
       'general-ocr-advanced',
@@ -21,203 +21,21 @@ export class LambdaContainersStack extends Stack {
         functionName: 'general-ocr-advanced',
         code: DockerImageCode.fromImageAsset('src/containers/general-ocr/model-advanced',
           {
-            file: 'Dockerfile.lambda'
-          }
+            file: 'Dockerfile.lambda',
+          },
         ),
       },
     );
 
-    const GeneralOCRStandard = new DockerImageFunction(
+    const GeneralOCRAdvancedSageMaker = new DockerImageFunction(
       this,
-      'general-ocr-standard',
+      'general-ocr-advanced-sm',
       {
-        functionName: 'general-ocr-standard',
-        code: DockerImageCode.fromImageAsset('src/containers/general-ocr/model-standard',
+        functionName: 'general-ocr-advanced-sm',
+        code: DockerImageCode.fromImageAsset('src/containers/general-ocr/model-advanced',
           {
-            file: 'Dockerfile.lambda'
-          }
-        ),
-      },
-    );
-
-    const GeneralOCRTraditionalStandard = new DockerImageFunction(
-      this,
-      'general-ocr-traditional-standard',
-      {
-        functionName: 'general-ocr-traditional-standard',
-        code: DockerImageCode.fromImageAsset('src/containers/general-ocr-traditional/model-standard',
-          {
-            file: 'Dockerfile.lambda'
-          }
-        ),
-      },
-    );
-
-    const NudityDetection = new DockerImageFunction(
-      this,
-      'pornography-detection',
-      {
-        functionName: 'pornography-detection',
-        code: DockerImageCode.fromImageAsset('src/containers/pornography-detection/model',
-          {
-            file: 'Dockerfile.lambda'
-          }
-        ),
-      },
-    );
-
-    const HumanImageSegmentation = new DockerImageFunction(
-      this,
-      'human-image-segmentation',
-      {
-        functionName: 'human-image-segmentation',
-        code: DockerImageCode.fromImageAsset('src/containers/human-image-segmentation/model',
-          {
-            file: 'Dockerfile.lambda'
-          }
-        ),
-      },
-    );
-
-    const ObjectRecognition = new DockerImageFunction(
-      this,
-      'object-recognition',
-      {
-        functionName: 'object-recognition',
-        code: DockerImageCode.fromImageAsset('src/containers/object-recognition/model',
-          {
-            file: 'Dockerfile.lambda'
-          }
-        ),
-      },
-    );
-
-    const FaceComparison = new DockerImageFunction(
-      this,
-      'face-comparison',
-      {
-        functionName: 'face-comparison',
-        code: DockerImageCode.fromImageAsset('src/containers/face-comparison/model',
-          {
-            file: 'Dockerfile.lambda'
-          }
-        ),
-      },
-    );
-
-    const FaceDetection = new DockerImageFunction(
-      this,
-      'face-detection',
-      {
-        functionName: 'face-detection',
-        code: DockerImageCode.fromImageAsset('src/containers/face-detection/model',
-          {
-            file: 'Dockerfile.lambda'
-          }
-        ),
-      },
-    );
-
-    const HumanAttribute = new DockerImageFunction(
-      this,
-      'human-attribute',
-      {
-        functionName: 'human-attribute',
-        code: DockerImageCode.fromImageAsset('src/containers/human-attribute/model',
-          {
-            file: 'Dockerfile.lambda'
-          }
-        ),
-      },
-    );
-
-    const LicensePlate = new DockerImageFunction(
-      this,
-      'car-license-plate',
-      {
-        functionName: 'car-license-plate',
-        code: DockerImageCode.fromImageAsset('src/containers/license-plate/model',
-          {
-            file: 'Dockerfile.lambda'
-          }
-        ),
-      },
-    );
-
-    const CustomOcr = new DockerImageFunction(
-      this,
-      'custom-ocr',
-      {
-        functionName: 'custom-ocr',
-        code: DockerImageCode.fromImageAsset('src/containers/custom-ocr/model',
-          {
-            file: 'Dockerfile.lambda'
-          }
-        ),
-      },
-    );
-
-    const TextSimilarity = new DockerImageFunction(
-      this,
-      'text-similarity',
-      {
-        functionName: 'text-similarity',
-        code: DockerImageCode.fromImageAsset('src/containers/text-similarity/model',
-          {
-            file: 'Dockerfile.lambda'
-          }
-        ),
-      },
-    );
-
-    const ImageSimilarity = new DockerImageFunction(
-      this,
-      'image-similarity',
-      {
-        functionName: 'image-similarity',
-        code: DockerImageCode.fromImageAsset('src/containers/image-similarity/model',
-          {
-            file: 'Dockerfile.lambda'
-          }
-        ),
-      },
-    );
-
-    const GreenScreenMatting = new DockerImageFunction(
-      this,
-      'green-screen-matting',
-      {
-        functionName: 'green-screen-matting',
-        code: DockerImageCode.fromImageAsset('src/containers/green-screen-matting/model',
-          {
-            file: 'Dockerfile.lambda'
-          }
-        ),
-      },
-    );
-
-    const GeneralOCRViet = new DockerImageFunction(
-      this,
-      'general-ocr-viet',
-      {
-        functionName: 'general-ocr-viet',
-        code: DockerImageCode.fromImageAsset('src/containers/general-ocr-viet/model-standard',
-          {
-            file: 'Dockerfile.lambda'
-          }
-        ),
-      },
-    );
-
-    const ImageSuperResolution = new DockerImageFunction(
-      this,
-      'image-super-resolution',
-      {
-        functionName: 'image-super-resolution',
-        code: DockerImageCode.fromImageAsset('src/containers/image-super-resolution/model',
-          {
-            file: 'Dockerfile.lambda'
-          }
+            file: 'Dockerfile.sagemaker',
+          },
         ),
       },
     );
