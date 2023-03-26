40ba879eb6b87889045177d6f063c63beddb6689
yike5460 <yike5460@163.com>
2023-03-26 16:54:42 +0800

    docs: BYOC sample

---
+++ applications/stable-diffusion-extensions/container/train.py
@@ -0,0 +1,68 @@
+import os
+import argparse
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+# Define the Stable Diffusion model
+class StableDiffusionModel(nn.Module):
+    def __init__(self, n_channels, n_blocks):
+        super(StableDiffusionModel, self).__init__()
+        self.n_channels = n_channels
+        self.n_blocks = n_blocks
+        self.blocks = nn.Sequential(*[nn.Conv2d(self.n_channels, self.n_channels, 3, padding=1) for _ in range(self.n_blocks)])
+
+    def forward(self, x):
+        return x + self.blocks(x)
+
+def train(model, device, train_loader, optimizer, criterion, epoch):
+    model.train()
+    for batch_idx, (data, _) in enumerate(train_loader):
+        data = data.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, data)
+        loss.backward()
+        optimizer.step()
+
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--epochs', type=int, default=10, help='Number of training epochs')
+    parser.add_argument('--batch-size', type=int, default=64, help='Batch size for training')
+    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
+    parser.add_argument('--n-channels', type=int, default=64, help='Number of channels in the model')
+    parser.add_argument('--n-blocks', type=int, default=4, help='Number of convolutional blocks in the model')
+    args = parser.parse_args()
+
+    # Set up device and data loaders
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    transform = transforms.Compose([transforms.Resize(32),
+                                    transforms.RandomHorizontalFlip(),
+                                    transforms.RandomCrop(32, padding=4),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+
+    # Initialize the model, optimizer, and loss function
+    model = StableDiffusionModel(n_channels=args.n_channels, n_blocks=args.n_blocks).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    criterion = nn.MSELoss()
+
+    # Train the model
+    for epoch in range(1, args.epochs + 1):
+        train(model, device, train_loader, optimizer, criterion, epoch)
+
+    # Save the trained model
+    model_dir = os.environ.get('SM_MODEL_DIR', './model')
+    torch.save(model.state_dict(), os.path.join(model_dir, 'model.pt'))
+
+if __name__ == '__main__':
+    main()
---
+++ applications/stable-diffusion-extensions/scripts/playground.py
@@ -0,0 +1,79 @@
+import boto3
+import sagemaker
+from sagemaker import get_execution_role
+from sagemaker.estimator import Estimator
+from sagemaker.tuner import IntegerParameter, CategoricalParameter, ContinuousParameter, HyperparameterTuner
+
+# Initial setup for SageMaker
+session = sagemaker.Session()
+role = get_execution_role()
+aws_region = boto3.Session().region_name
+
+# Create ECR repository, we will not build the image, we will use the pre-built image
+ecr = boto3.client('ecr')
+
+# Skip the creation of the repository if it already exists
+try:
+    ecr.describe_repositories(repositoryNames=['sd-extensions'])
+except ecr.exceptions.RepositoryNotFoundException:
+    ecr.create_repository(repositoryName='sd-extensions')
+container_image_uri = ecr.describe_repositories(repositoryNames=['sd-extensions'])['repositories'][0]['repositoryUri']
+print(container_image_uri)
+
+# Sample training data is available in this bucket
+training_data_bucket = f"jumpstart-cache-prod-{aws_region}"
+training_data_prefix = "training-datasets/dogs_sd_finetuning/"
+
+training_dataset_s3_path = f"s3://{training_data_bucket}/{training_data_prefix}"
+
+output_bucket = session.default_bucket()
+output_prefix = "jumpstart-example-sd-training"
+
+s3_output_location = f"s3://{output_bucket}/{output_prefix}/output"
+
+# Create an Estimator instance for model training
+estimator = Estimator(image_uri=container_image_uri,
+                      role=role,
+                      instance_count=1,
+                      instance_type='ml.m5.large',
+                      base_job_name='stable-diffusion-model-training')
+
+###############################################################
+
+# Start the training job
+estimator.fit({'train': training_dataset_s3_path}, wait=False)
+
+###############################################################
+
+# Configure hyperparameter tuning (replace with the specific hyperparameters for your algorithm)
+hyperparameter_ranges = {'learning_rate': ContinuousParameter(0.001, 0.1)}
+
+# Set the objective metric
+objective_metric_name = 'validation:accuracy'
+
+# Create a HyperparameterTuner instance
+tuner = HyperparameterTuner(estimator,
+                            objective_metric_name,
+                            hyperparameter_ranges,
+                            max_jobs=10,
+                            max_parallel_jobs=2)
+
+# Launch the hyperparameter tuning job
+tuner.fit({'train': training_dataset_s3_path})
+
+# Wait for the tuning to complete
+tuner.wait()
+
+###############################################################
+
+# Deploy the best model to an endpoint
+predictor = tuner.deploy(initial_instance_count=1, instance_type='ml.m5.large')
+
+# Perform real-time inference with the deployed model (replace with the appropriate input for your algorithm)
+sample_data = 'your-input-data-for-inference'
+response = predictor.predict(sample_data)
+
+###############################################################
+
+# Optional: Delete the endpoint after use
+predictor.delete_endpoint()
