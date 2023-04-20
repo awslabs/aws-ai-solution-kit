528632a3069d6640e49c133d62e5c53132290ca0
Tang Jie <tangjiee@amazon.com>
2023-04-20 13:48:29 +0000

    Update create model test.

--- tests/db_create_model/test_db_create_model_ep.py
+++ tests/db_create_model/test_db_create_model_ep.py
@@ -51,6 +51,22 @@ if __name__ == '__main__':
         }),
     }
 
+    payload = {
+        "task": "db-create-model",
+        "db_create_model_payload": json.dumps({
+            "s3_output_path": ["alvindaiyan-aigc-testing-cloudfront-ext/dreambooth/"],
+            "s3_input_path": ["alvindaiyan-aigc-testing-cloudfront-ext/dreambooth/model/db_test_4/45ed2d86-561a-45f2-acd0-b53f9f65ec8e"],
+            "param": {
+                "create_model_params": {
+                    "new_model_name": "db_test_4",
+                    "ckpt_path": "v1-5-pruned-emaonly.safetensors",
+                }
+        },
+            "job_id": "b802b8bc-9e21-4ebb-b13e-4d07afa7b405"})}
+    payload = {'task': 'db-create-model', 'username': None, 'models': None, 'txt2img_payload': None, 'controlnet_txt2img_payload': None, 'img2img_payload': None, 'extras_single_payload': None, 'extras_batch_payload': None,
+               'db_create_model_payload': '{"s3_output_path": "alvindaiyan-aigc-testing-cloudfront-ext/dreambooth/", "s3_input_path": "s3://alvindaiyan-aigc-testing-cloudfront-ext/dreambooth/model/db_test_5/16506f4c-9051-4552-b7c9-32004a9a3ed0", "param": {"create_model_params": {"extract_ema": false, "from_hub": false, "new_model_name": "db_test_5", "ckpt_path": "v1-5-pruned-emaonly.safetensors", "train_unfrozen": false, "new_model_url": "", "is_512": true, "new_model_token": ""}}, "job_id": "16506f4c-9051-4552-b7c9-32004a9a3ed0"}'}
+    payload = {'task': 'db-create-model', 'username': None, 'models': None, 'txt2img_payload': None, 'controlnet_txt2img_payload': None, 'img2img_payload': None, 'extras_single_payload': None, 'extras_batch_payload': None, 'db_create_model_payload': '{"s3_output_path": "alvindaiyan-aigc-testing-cloudfront-ext/dreambooth/", "s3_input_path": "s3://alvindaiyan-aigc-testing-cloudfront-ext/dreambooth/model/db_test_5/655c2a6f-e9a3-4cfe-a7ee-d25e291539e8", "param": {"create_model_params": {"extract_ema": false, "from_hub": false, "new_model_name": "db_test_5", "ckpt_path": "v1-5-pruned-emaonly.safetensors xxxx", "train_unfrozen": false, "new_model_url": "", "is_512": true, "new_model_token": ""}}, "job_id": "655c2a6f-e9a3-4cfe-a7ee-d25e291539e8"}'}
+    payload = {'task': 'db-create-model', 'username': None, 'models': None, 'txt2img_payload': None, 'controlnet_txt2img_payload': None, 'img2img_payload': None, 'extras_single_payload': None, 'extras_batch_payload': None, 'db_create_model_payload': '{"s3_output_path": "alvindaiyan-aigc-testing-cloudfront-ext/dreambooth/", "s3_input_path": "s3://alvindaiyan-aigc-testing-cloudfront-ext/dreambooth/model/db_test_5/c9f20b15-76c1-4c2c-8560-3cc4349928f6", "param": {"create_model_params": {"extract_ema": false, "from_hub": false, "new_model_name": "db_test_6", "ckpt_path": "v1-5-pruned-emaonly.safetensors", "train_unfrozen": false, "new_model_url": "", "is_512": true, "new_model_token": ""}}, "job_id": "c9f20b15-76c1-4c2c-8560-3cc4349928f6"}'}
     # db_create_model_params = json.loads(payload['db_create_model_payload'])
     # local_model_dir = f'models/Stable-diffusion/{db_create_model_params["new_model_src"]}'
     # bucket_name = db_create_model_params['bucket_name']
@@ -63,7 +79,7 @@ if __name__ == '__main__':
     # endpoint_name = "aigc-webui-dreambooth-create-model-2023-04-13-09-21-31-981"
     # endpoint_name = "db-create-model-1681437544-456743"
     # endpoint_name = "aigc-createmodel-endpoint"
-    endpoint_name = "db-create-model-1681723003-1839077"
+    endpoint_name = "db-create-model-1681984053-4592674"
 
     predictor = Predictor(endpoint_name)
 
