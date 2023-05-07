9ba48387aa6befea3b10062725445e99b037366e
George Gu <minggu@amazon.com>
2023-05-07 02:33:33 +0000

    [fix]adding api_key requirement for test-connection method

--- infrastructure/src/sd-inference/sd-async-inference-stack.ts
+++ infrastructure/src/sd-inference/sd-async-inference-stack.ts
@@ -300,6 +300,7 @@ export class SDAsyncInferenceStack extends Stack {
                     },
                 },
             ],
+            apiKeyRequired: true,
         });
 
         const current_time = new Date().toISOString;
